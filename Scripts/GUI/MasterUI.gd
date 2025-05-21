bfef4e8632112e0ff48684da1fa9d4026141fdf2
VinceTrl <v.trinel.design@gmail.com>
2025-05-21 20:04:24 +0200

    Import Enviro in game scene

--- Scripts/GUI/MasterUI.gd
+++ Scripts/GUI/MasterUI.gd
@@ -1,9 +1,10 @@
 class_name MasterUI
 extends Control
 
-@onready var fight_text: FightText = $FightText
-@onready var timer_root: Control = $TimerRoot
-@onready var title_screen: TitleScreen = $TitleScreen
+
+@onready var fight_text: FightText = $CanvasLayer/FightText
+@onready var timer_root: Control = $CanvasLayer/TimerRoot
+@onready var title_screen: TitleScreen = $CanvasLayer/TitleScreen
 
 
 func _ready():
