14d5502e222f51b20c3e4698bdcaa755a7f3f220
VinceTrl <v.trinel.design@gmail.com>
2025-06-25 13:57:34 +0200

    HP GUI integration

--- Scripts/GUI/texture_hp.gd
+++ Scripts/GUI/texture_hp.gd
@@ -1,7 +1,9 @@
-
-extends TextureRect
+class_name PlayerGUI_HP
+extends AnimatedSprite2D
 
 #@export var textureHP: CompressedTexture2D = preload("res://Assets/Sprites/GUI/KikiGUI/GUI_LifePoint_P1.png")
+@export var playerIndex = 1
+var isActive = true
 
 func _ready():
 	pass
@@ -10,3 +12,26 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func SetHpIdle():
+	print("SET IDLE IN HP GUI")
+	isActive = true
+	if(playerIndex == 1):
+		play("P1_Idle")
+	else:
+		play("P2_Idle")
+	await animation_finished
+	#visible = true
+
+func RemoveHp():
+	if(!isActive): return
+	
+	isActive = false
+	print("REMOVE HP GUI")
+	if(playerIndex == 1):
+		play("P1_Hit")	
+	else:
+		play("P2_Hit")
+		
+	await animation_finished
+	#visible = false
