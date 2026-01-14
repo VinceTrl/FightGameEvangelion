4246037c2c210626c8e6ce49b17e5d69571dceac
VinceTrl <v.trinel.design@gmail.com>
2026-01-14 01:00:33 +0100

    loading optim

---
+++ Scripts/Managers/Global/map_global.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+const EVANGELION_MAP = preload("res://Scenes/Maps/Maps/evangelion_map.tscn")
+const TEST_MAP = preload("res://Scenes/Maps/Maps/Test_map.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func InstantiateMap(mapName:String) -> Node:
+	var instance
+	match mapName:
+		"Evangelion":
+			print("eva map instance")
+			instance = EVANGELION_MAP.instantiate()
+		"Beach":
+			print("beach map instance")
+			instance = TEST_MAP.instantiate()
+
+	return instance
