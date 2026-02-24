5de39ae414eb1e1781c78bc28a3c584bc1b67a60
VinceTrl <v.trinel.design@gmail.com>
2026-02-25 00:52:00 +0100

    New silent hill map added

--- Scripts/Managers/Global/map_global.gd
+++ Scripts/Managers/Global/map_global.gd
@@ -3,6 +3,7 @@ extends Node
 
 const EVANGELION_MAP = preload("res://Scenes/Maps/Maps/evangelion_map.tscn")
 const TEST_MAP = preload("res://Scenes/Maps/Maps/Test_map.tscn")
+const SILENT_HILL_MAP = preload("res://Scenes/Maps/Maps/SilentHill_map.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,5 +23,8 @@ func InstantiateMap(mapName:String) -> Node:
 		"Beach":
 			print("beach map instance")
 			instance = TEST_MAP.instantiate()
+		"SilentHill":
+			print("Silent Hill map instance")
+			instance = SILENT_HILL_MAP.instantiate()
 
 	return instance
